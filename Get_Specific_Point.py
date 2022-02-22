3fd8946815c50672da33ca4ee658b6fff91ca811
EternalBlue3 <100178397+EternalBlue3@users.noreply.github.com>
2022-02-21 23:12:03 -0500

    Update Get_Specific_Point.py

--- Get_Specific_Point.py
+++ Get_Specific_Point.py
@@ -2,13 +2,16 @@ def isEven(n):
     return (n % 2 == 0)
 
 h = int(input("Enter a number to start the Syracuse Conjecture with: "))
+output = []
+four_two_one = ["4","2","1"]
 iterations = 0
 while h != 4:
     iterations += 1
-    print(h)
+    output.append(str(h))
     if isEven(h):
         h = h // 2
     else:
         h = h * 3 + 1
-print("4\n2\n1") 
-print("There were "+str(iterations + 2)+" iterations.")
+output = ','.join(output), ','.join(four_two_one)
+print("Output: ",','.join(output))
+print("There were "+str(iterations + 2)+" steps.")
