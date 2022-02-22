fd8ffd2f09e5f7f1aa25e91a2d00dd0bdf27746a
EternalBlue3 <100178397+EternalBlue3@users.noreply.github.com>
2022-02-21 22:51:54 -0500

    Create Generate_Full_List.py

---
+++ Generate_Full_List.py
@@ -0,0 +1,27 @@
+def isEven(n):
+    return (n % 2 == 0)
+
+full_list = []
+
+h = 10
+v = 0
+for i in range(int(input("How many iterations of the Collutz"))):
+    iterations = 0
+    i += 1
+    v += 1
+    h = 10
+    while h == 10:
+        if i == 1:
+            h = 0
+        iterations += 1
+        if isEven(i):
+            i = i // 2
+        else:
+            i = i * 3 + 1
+    a = "Checked ", str(v), " This took "+str(iterations - 1)+" iterations\n"
+    a = ''.join(a)
+    full_list.append(a)
+complete = ''.join(full_list)
+fh = open("Syracuse Conjecture file.txt","w")
+fh.write(complete)
+fh.close()
